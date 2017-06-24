4abc9c1f01e20a127745986449e91d985e35aed1
LDoolittle <LDoolittle@musicnotes.com>
2017-06-24 15:32:01 -0500

    updates

--- exercise_1/transforming/create_metrics.sql
+++ exercise_1/transforming/create_metrics.sql
@@ -16,4 +16,26 @@ cast(concat(
 	 '-', 
 	substr(measure_end_date, 4, 2))
 as date) end_date
-FROM measures;
+FROM measures
+
+UNION
+SELECT 
+measure_id metric_id,
+measure_name description,
+cast(concat(
+	substr(measure_start_date, 7, 4), 
+	'-', 
+	substr(measure_start_date, 1, 2),
+	 '-', 
+	substr(measure_start_date, 4, 2))
+as date) start_date,
+cast(concat(
+	substr(measure_end_date, 7, 4), 
+	'-', 
+	substr(measure_end_date, 1, 2),
+	 '-', 
+	substr(measure_end_date, 4, 2))
+as date) end_date
+FROM infections
+GROUP BY measure_id, measure_name, measure_start_date, measure_end_date
+;
