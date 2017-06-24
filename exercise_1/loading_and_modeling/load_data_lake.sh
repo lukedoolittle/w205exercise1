57669b0dc9bec5a16bd761d75295d6c32734e8f4
LDoolittle <LDoolittle@musicnotes.com>
2017-06-24 09:58:13 -0500

    fixed load data lake so it was more iterative

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -12,63 +12,43 @@ cd ~/staging/exercise_1
 wget https://data.medicare.gov/views/bg9k-emty/files/0a9879e0-3312-4719-a1db-39fd114890f1?filename=Hospital_Revised_Flatfiles.zip -O Hospital_Revised_Flatfiles.zip
 unzip Hospital_Revised_Flatfiles.zip -d Hospital_Revised_Flatfiles
 
-declare -A files = (["hospitals"] = "Hospital General Information.csv")
+# create a mapping of all the physical file names to their logical names
+declare -A file_mapping=( \
+["hospitals"]="Hospital General Information.csv" \
+["effective_care"]="Timely and Effective Care - Hospital.csv" \
+["readmissions"]="Readmissions and Deaths - Hospital.csv" \
+["measures"]="Measure Dates.csv" \
+["survey_responses"]="hvbp_hcahps_11_10_2016.csv" \
+["complications"]="Complications - Hospital.csv" \
+["infections"]="Healthcare Associated Infections - Hospital.csv" \
+["imaging"]="Outpatient Imaging Efficiency - Hospital.csv" \
+["structural_measures"]="Structural Measures - Hospital.csv" \
+["readmission_reduction"]="READMISSION REDUCTION.csv" \
+["heart_attack_scores"]="hvbp_ami_11_14_2016.csv" \
+["outcome_scores"]="hvbp_clinical_care_outcomes_11_10_2016.csv" \
+["immunization_scores"]="hvbp_imm2_11_10_2016.csv" \
+["preventative_scores"]="hvbp_pc_11_10_2016.csv" \
+["safety_scores"]="hvbp_safety_11_10_2016.csv" \
+["efficiency_scores"]="hvbp_efficiency_11_10_2016.csv" \
+["total_scores"]="hvbp_tps_11_10_2016.csv" \
+)
 
-
-# remove the column labels (the first line) from each file and rename
-tail -n +2 "Hospital_Revised_Flatfiles/Hospital General Information.csv" > hospitals.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Timely and Effective Care - Hospital.csv" > effective_care.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Readmissions and Deaths - Hospital.csv" > readmissions.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Measure Dates.csv" > measures.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_hcahps_11_10_2016.csv" > survey_responses.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Complications - Hospital.csv" > complications.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Healthcare Associated Infections - Hospital.csv" > infections.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Outpatient Imaging Efficiency - Hospital.csv" > imaging.csv
-tail -n +2 "Hospital_Revised_Flatfiles/Structural Measures - Hospital.csv" > structural_measures.csv
-tail -n +2 "Hospital_Revised_Flatfiles/READMISSION REDUCTION.csv" > readmission_reduction.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_ami_05_28_2015.csv" > heart_attack_scores.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_hai_05_28_2015.csv" > surgery_infections_scores.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_hf_05_28_2015.csv" > heart_failure_scores.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_pn_05_28_2015.csv" > pnumenoia_scores.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_scip_05_28_2015.csv" > surgery_scores.csv
-tail -n +2 "Hospital_Revised_Flatfiles/hvbp_outcome_05_18_2015.csv" > outcome_scores.csv
-
-# create the root directory and each subdirectory in HSFS
+# create the root directory in HDFS
 hdfs dfs -mkdir /user/w205/hospital_compare
-hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
-hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
-hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
-hdfs dfs -mkdir /user/w205/hospital_compare/measures
-hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
-hdfs dfs -mkdir /user/w205/hospital_compare/complications
-hdfs dfs -mkdir /user/w205/hospital_compare/infections
-hdfs dfs -mkdir /user/w205/hospital_compare/imaging
-hdfs dfs -mkdir /user/w205/hospital_compare/structural_measures
-hdfs dfs -mkdir /user/w205/hospital_compare/readmission_reduction
-hdfs dfs -mkdir /user/w205/hospital_compare/heart_attack_scores
-hdfs dfs -mkdir /user/w205/hospital_compare/surgery_infections_scores
-hdfs dfs -mkdir /user/w205/hospital_compare/heart_failure_scores
-hdfs dfs -mkdir /user/w205/hospital_compare/pnumenoia_scores
-hdfs dfs -mkdir /user/w205/hospital_compare/surgery_scores
-hdfs dfs -mkdir /user/w205/hospital_compare/outcome_scores
 
-# copy each of the csv files into HDFS
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
-hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
-hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
-hdfs dfs -put measures.csv /user/w205/hospital_compare/measures
-hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
-hdfs dfs -put complications.csv /user/w205/hospital_compare/complications
-hdfs dfs -put infections.csv /user/w205/hospital_compare/infections
-hdfs dfs -put imaging.csv /user/w205/hospital_compare/imaging
-hdfs dfs -put structural_measures.csv /user/w205/hospital_compare/structural_measures
-hdfs dfs -put readmission_reduction.csv /user/w205/hospital_compare/readmission_reduction
-hdfs dfs -put heart_attack_scores.csv /user/w205/hospital_compare/heart_attack_scores
-hdfs dfs -put surgery_infections_scores.csv /user/w205/hospital_compare/surgery_infections_scores
-hdfs dfs -put heart_failure_scores.csv /user/w205/hospital_compare/heart_failure_scores
-hdfs dfs -put pnumenoia_scores.csv /user/w205/hospital_compare/pnumenoia_scores
-hdfs dfs -put surgery_scores.csv /user/w205/hospital_compare/surgery_scores
-hdfs dfs -put outcome_scores.csv /user/w205/hospital_compare/outcome_scores
+# go through all the mappings and 
+# (1) remove the header (the first line) from each
+# (2) create the appropriate subdirectory in HDFS
+# (3) place the modified file in the new subdirectory
+for name in "${!file_mapping[@]}";
+do tail -n +2 "Hospital_Revised_Flatfiles/${file_mapping[$name]}" > "$name.csv";
+hdfs dfs -mkdir /user/w205/hospital_compare/"$name";
+hdfs dfs -put "$name".csv /user/w205/hospital_compare/"$name"
+done
+
+#clean up now unneeded files
+rm -r Hospital_Revised_Flatfiles
+rm Hospital_Revised_Flatfiles.zip
 
 # change directory back to original
 cd $MY_CWD
