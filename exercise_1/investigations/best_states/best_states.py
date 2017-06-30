1186d11ed51e6c560a8e5105c48bf59c31d97b62
LDoolittle <LDoolittle@musicnotes.com>
2017-06-30 11:50:36 -0500

    some formatting updates

--- exercise_1/investigations/best_states/best_states.py
+++ exercise_1/investigations/best_states/best_states.py
@@ -2,12 +2,21 @@ from pyspark.sql.types import *
 from pyspark.sql import HiveContext
 import decimal
 
-context = HiveContext(sc)
+# Generate the scaled scores table
+execfile("../core/create_scaled_scores.py")
 
-nonStates = ['PR', 'VI', 'SD', 'AS', 'GU', 'MP']
+nonStates = ['PR', 
+			 'VI', 
+			 'SD', 
+			 'AS', 
+			 'GU', 
+			 'MP']
 
 # Utilize the temporary table to create the average and variance of each scaled score by state
 # In addition filter out non-states such as Guam
-resultSql = 'select state, avg(scaled_score) mean_scaled_score, sum(scaled_score) sum_scaled_score, var_samp(scaled_score) variance_scaled_score, count(*) score_count from intermediate_table join providers on intermediate_table.provider_id = providers.provider_id where state not in ({0}) group by state order by mean_scaled_score desc'.format(','.join(['\'' + member + '\'' for member in nonStates]))
-result = context.sql(resultSql)
+resultSql = ('select state, avg(scaled_score) mean_scaled_score, sum(scaled_score) sum_scaled_score, var_samp(scaled_score) variance_scaled_score, count(*) score_count from scaled_scores join providers on intermediate_table.provider_id = providers.provider_id where state not in ({0}) group by state order by mean_scaled_score desc'
+			.format(','.join(['\'' + member + '\'' 
+							 for member 
+							 in nonStates])))
+result = sqlContext.sql(resultSql)
 result.show(10, False)
--- exercise_1/investigations/hospital_variability/hospital_variability.py
+++ exercise_1/investigations/hospital_variability/hospital_variability.py
@@ -2,13 +2,12 @@ from pyspark.sql.types import *
 from pyspark.sql import HiveContext
 import decimal
 
-context = HiveContext(sc)
+# Get the total count of providers scored
+providerCount = sqlContext.sql('select count(*) from providers').first()[0]
 
-# Get the variance relative to the mean for only those scores that
-# Have ratings for more than 1/4 of the providers
-# Also we need to filter out any of the HAI scores since they (appear to)
-# represent number of days/patients which isn't a relevant parameter
-providerCount = context.sql('select count(*) from providers').first()[0]
+# Get the variance relative to the mean for only those scores that have ratings 
+# for more than 1/4 of the providers. Also filter out any of the HAI scores 
+# since they (appear to) represent an irrelevant relevant parameter
 resultSql = 'select description, score_variance_mean_relative from (select metric_id, count(*) score_count, var_samp(score) / avg(score) score_variance_mean_relative from scores group by metric_id) a join metrics on a.metric_id = metrics.metric_id where score_count > {0} and a.metric_id not like \'HAI%\' order by score_variance_mean_relative desc'.format(providerCount / 4)
-result = context.sql(resultSql)
+result = sqlContext.sql(resultSql)
 result.show(10, False)
